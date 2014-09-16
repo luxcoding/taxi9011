eebd7be8c37bb8b41c365f78e4081f4b8e6fe92e
Georgiy <goshazvir@gmail.com>
2014-09-16 14:11:24 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
 head.ready(function() {
+	$('.scroll-pane').jScrollPane();
 
+	$(window).resize(function(){
+	  if ($(".scroll-pane").length > 0){
+	    $('.scroll-pane').jScrollPane();
+	  }
+	});
 });
